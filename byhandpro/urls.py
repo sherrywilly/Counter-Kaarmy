ecd4cde0b48ac63dce55b3171f0980060bcdd02d
sherrywilly <sherrywilson521@gmail.com>
2021-06-08 19:12:00 +0530

    requirements included

--- byhandpro/urls.py
+++ byhandpro/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from counter.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('table/', employee_table, name='table'),
+    # path('table/', employee_table, name='table'),
     path('counter/', include('counter.urls')),
-    path('', employee_manage, name='manage'),
-  
+    # path('', employee_manage, name='manage'),
+
 ]
